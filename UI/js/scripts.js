05711ca2951b0d29e419d5cdbfe8970a0dbfb708
Aleksey Afanasenko <alex15@open.by>
2016-03-20 22:32:17 +0300

    [Task 5] Remove jquery, add work with localstorage

--- UI/js/scripts.js
+++ UI/js/scripts.js
@@ -1,146 +1,229 @@
-var author = "Guest";
-var messageField = $('#message');
-var messageContainer = $('#messages-container');
-var usersList = $('#users-list');
-var messageForm = $('#message-form');
+var messagesKey = "TypeWriter Messages";
+var authorKey = "TypeWriter Author";
 
-function htmlentities(s){
-    var pre = document.createElement('pre');
-    var text = document.createTextNode(s);
-    pre.appendChild(text);
-    return pre.innerHTML;
+var author = "Guest";
+var messageList = [];
+
+
+function run(){
+    document.getElementById("message-form").addEventListener("submit", function(e){
+        e.preventDefault();
+        send();
+    });
+
+    document.getElementById("message-form").addEventListener("keypress", keypressOnForm);
+
+    document.getElementById("messages-container").addEventListener("click", delegateEvent);
+
+    loadFromStorage();
+    render();
+}
+
+function delegateEvent(evt){
+    if(evt.target.classList.contains('edit')){
+        edit(evt.target.parentNode.parentNode.parentNode.parentNode);
+    } else if(evt.target.classList.contains('delete')){
+        remove(evt.target.parentNode.parentNode.parentNode.parentNode);
+    }
+}
+
+function edit(messageWrapper){
+    var message = messageList[indexByElement(messageWrapper, messageList)];
+	swal({
+        title: "Edit your message!",
+        text: "<textarea id='message-field' style='width:100%;resize:none;' rows='3'>" + message.message +"</textarea>",
+        html: true,
+        showCancelButton: true,
+        closeOnConfirm: false,
+        animation: "slide-from-top"},
+        function(){
+            var messageValue = document.getElementById('message-field').value;
+            if (messageValue === false) return false;
+            if (messageValue === "") {
+                swal.showInputError("You need to write something!");
+                return false;
+            }
+            if (messageValue === message.message){
+                swal.close();
+                return false;
+            }
+            message.message = htmlentities(messageValue);
+            message.edit = formatDate(new Date());
+            updateMessage(message);
+            swal.close();
+        }
+    );
+}
+
+function remove(messageWrapper){
+    var message = messageList[indexByElement(messageWrapper, messageList)];
+    message.remove = true;
+    updateMessage(message);
+}
+
+function indexByElement(element, messages){
+    var id = element.attributes['data-id'].value;
+    return messages.findIndex(function(item){
+        return item.id == id;
+    })
+}
+
+function keypressOnForm(e){
+    var keyCode = (e.keyCode ? e.keyCode : e.which);
+    if(!e.ctrlKey && keyCode == 13){
+        e.preventDefault();
+        send();
+    }
+    else if((keyCode == 10 || keyCode == 13) && e.ctrlKey){
+        var field = e.target;
+        field.value += "\n";
+        field.scrollTop = field.scrollHeight;
+    }
+}
+
+function getMessage(message){
+    message = cloneObject(message);
+    var messageTemplate = document.getElementById('message-template').innerHTML;
+    message.ctrl = "";
+    message.editInfo = "";
+    message.class = "";
+    if(message.isMy){
+        message.ctrl = document.getElementById("ctrl-template").innerHTML;
+        message.class = "my-message";
+    }
+    if(message.edit !== false){
+        var editInfoTemplate = document.getElementById('edit-info-template').innerHTML;
+        message.editInfo = editInfoTemplate.supplant({date : message.edit});
+    }
+    if(message.remove){
+        message.class += " deleted";
+        message.message = "This message has been removed.";
+        message.ctrl = "";
+        message.editInfo = "";
+    }
+    return messageTemplate.supplant(message);
+}
+
+function renderMessage(message){
+    var messagesContainer = document.getElementById('messages-container');
+    var messageWrapper = document.createElement('div');
+    messageWrapper.classList.add("message-wrapper");
+    messageWrapper.setAttribute('data-id', message.id);
+    messageWrapper.innerHTML = getMessage(message);
+    messagesContainer.appendChild(messageWrapper);
+    messageWrapper.scrollIntoView();
+}
+
+function render(){
+    for(var i = 0; i < messageList.length; i++){
+        renderMessage(messageList[i]);
+    }
+    renderName(author);
+}
+
+function updateMessage(message){
+    saveMessages(messageList);
+    var messageWrapper = document.querySelector('[data-id="' + message.id + '"]');
+    messageWrapper.innerHTML = getMessage(message);
+}
+
+function newMessage(messageText, messageAuthor){
+    var my = false;
+    if (messageAuthor === undefined){
+        my = true;
+        messageAuthor = author;
+    }
+    return {
+        id: guid(),
+        author: messageAuthor,
+        date: formatDate(new Date()),
+        message: htmlentities(messageText),
+        edit: false,
+        remove: false,
+        isMy: my
+    };
 }
 
 function send(){
-    if(messageField.val().length == 0){
-        sweetAlert(
+    var messageField = document.getElementById('message');
+    if(messageField.value.length == 0){
+        swal(
             'Oops...',
             'Message can not be empty!',
             'error'
         );
         return;
     }
-    var message = '<div class="message my-message">' + htmlentities(messageField.val()) + '</div></div>';
-    var date = new Date();
-    var controls = '<a href="#" class="edit" title="Edit message"><i class="fa fa-pencil"></i></a> ' +
-        '<a href="#" class="delete" title="Remove message"><i class="fa fa-trash"></i></a>';
-    var authorInfo = '<div class="author-info">' + formatDate(date) + ' ' + author + ' ' + controls + '</div>';
-    var messageInfo = '<div class="message-info me">' + authorInfo + '</div>';
-    messageField.val("");
-    messageContainer.append(
-      '<div class="message-wrapper">' + messageInfo + message + '</div>'
-    );
-    scrollDownMessages();
+    var message = newMessage(messageField.value);
+    messageField.value = "";
+    messageList.push(message);
+    saveMessages(messageList);
+    renderMessage(message);
 }
 
 function changeName(){
     swal({
         title: "Change name!",
-        text: "Write your new name:",
+        text: "Write your name:",
         type: "input",
         showCancelButton: true,
         closeOnConfirm: false,
         animation: "slide-from-top",
         inputPlaceholder: "New name" },
         function(inputValue){
-            if (inputValue === false) return false;
-            if (inputValue === "") {
+            if(inputValue === false) return false;
+            if(inputValue === ""){
                 swal.showInputError("You need to write something!");
                 return false;
             }
-            if (inputValue.length > 30) {
+            if(inputValue.length > 30){
                 swal.showInputError("The name may not be greater than 30 characters.");
                 return false;
             }
-            author = inputValue;
-            $('#me').html(author);
+            author = htmlentities(inputValue);
+            saveAuthor(author);
+            renderName(author);
             swal.close();
-        });
-}
-
-function formatDate(date){
-    var minutes = date.getMinutes();
-    if (minutes < 10) minutes = '0' + minutes;
-
-    var hh = date.getHours();
-    if (hh < 10) hh = '0' + hh;
-
-    var dd = date.getDate();
-    if (dd < 10) dd = '0' + dd;
-
-    var mm = date.getMonth() + 1;
-    if (mm < 10) mm = '0' + mm;
-
-    var yy = date.getFullYear();
-
-    return dd + '.' + mm + '.' + yy + ' ' + hh + ':' + minutes;
-}
-
-function toggleUsers(){
-    usersList.toggleClass('hidden-mobile');
-}
-
-function scrollDownMessages(){
-    messageContainer.scrollTop(messageContainer[0].scrollHeight);
-}
-
-$(function(){
-    scrollDownMessages();
-
-    messageForm.submit(function(e){
-        e.preventDefault();
-        send();
-    });
-
-    messageField.keypress(function(e){
-        var keyCode = (e.keyCode ? e.keyCode : e.which);
-        if (!e.ctrlKey && keyCode == 13) {
-            e.preventDefault();
-            messageForm.submit();
         }
-        else if((keyCode == 10 || keyCode == 13) && e.ctrlKey){
-            messageField.val(messageField.val() + "\n");
-        }
-    });
+    );
+}
 
-    $(document).on('click', '.delete', function(){
-        var message =  $(this).parent().parent().next();
-        $(this).prev().remove();
-        $(this).remove();
-        message.html("This message has been removed.");
-        message.addClass("deleted");
-    });
+function renderName(author){
+    document.getElementById("me").innerHTML = author;
+}
 
-    $(document).on('click', '.edit', function(){
-        var messageWrapper = $(this).parent().parent();
-        var message = messageWrapper.next();
-        var messageText = message.html()
-        swal({
-            title: "Edit your message!",
-            text: "<textarea id='messageField' style='width:100%;resize:none;' rows='3'>" + messageText +"</textarea>",
-            html: true,
-            showCancelButton: true,
-            closeOnConfirm: false,
-            animation: "slide-from-top"},
-            function(){
-                var messageValue = $('#messageField').val();
-                if (messageValue === false) return false;
-                if (messageValue === "") {
-                    swal.showInputError("You need to write something!");
-                    return false;
-                }
-                if (messageValue === messageText){
-                    swal.close();
-                    return false;
-                }
-                message.html(htmlentities(messageValue));
-                var messageInfo = messageWrapper.children();
-                messageInfo.next().remove();
-                var editInfo = "Edited  " + formatDate(new Date());
-                messageInfo.parent().append('<div class="edit-info">' + editInfo + '</div>');
-                swal.close();
-            }
-        );
-    });
+function toggleSidebar(){
+    var sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle('hidden-mobile');
+}
 
-});
+function checkStorage(){
+    if(typeof(Storage) == "undefined"){
+        alert('localStorage is not accessible');
+    }
+}
+
+function saveMessages(messages){
+    checkStorage();
+    localStorage.setItem(messagesKey, JSON.stringify(messages));
+}
+
+function saveAuthor(author){
+    checkStorage();
+    localStorage.setItem(authorKey, author);
+}
+
+function loadFromStorage(){
+    checkStorage();
+    messageList = localStorage.getItem(messagesKey);
+    if(!messageList){
+        messageList = [newMessage("Welcome to our chat!", "System")]
+    }
+    else{
+        messageList = JSON.parse(messageList)
+    }
+    author = localStorage.getItem(authorKey);
+    if(!author){
+        author = "Guest";
+    }
+}
